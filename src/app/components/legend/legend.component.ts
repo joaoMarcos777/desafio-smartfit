3dc1ed8ffdc3d4adff10177744d6f9b18899e4f0
João Marcos <joaomarcos@frwk.com.br>
2024-11-05 01:04:38 -0300

    feat: add legend component

---
+++ src/app/components/legend/legend.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legend',
+  standalone: true,
+  templateUrl: './legend.component.html',
+  styleUrls: ['./legend.component.scss'],
+})
+export class LegendComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
